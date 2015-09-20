bba2c43d8a5133ad2d1ee425af0d7f3f2ee122dd
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-09-20 09:58:25 +0200

    vector

---
+++ vector.h
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <assert.h>
+
+typedef struct {
+	E *data;
+	size_t length;
+} Vector;
+
+Vector *vector_new()
+{
+	Vector *o = malloc(sizeof(*o));
+	o->length = 0;
+	o->data = NULL;
+	return o;
+}
+
+void vector_free(Vector *o)
+{
+	if (o->data) {
+		free(o->data);
+		o->data = NULL;
+	}
+	free(o);
+}
+
+void vector_push(Vector *o, E element)
+{
+	o->data = realloc(o->data, ++o->length * sizeof(E));
+	assert(o->data);
+	o->data[o->length - 1] = element;
+}
+
+E vector_pop(Vector *o)
+{
+	assert(o->length > 0);
+	E ret = o->data[--o->length];
+	if (o->length > 0) {
+		o->data = realloc(o->data, o->length * sizeof(E));
+	}
+	return ret;
+}
+
---
+++ vector_test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "asrt/asrt.h"
+
+#define E int
+#include "vector.h"
+#undef E
+
+#define MEGA (1024 * 1024)
+
+int main(int argc, char const *argv[])
+{
+	Vector *vector = vector_new();
+	for (int i = 0; i < 10 * MEGA; i++) {
+		vector_push(vector, i);
+	}
+	for (int i = 10 * MEGA - 1; i >= 0; i--) {
+		int e = vector_pop(vector);
+		ASRT_int(i, e);
+	}
+	vector_free(vector);
+	printf("All Ok.\n");
+	return 0;
+}
