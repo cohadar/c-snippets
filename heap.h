ed64caf54154ecc3357da8c32baabedac9252986
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-09-25 10:03:30 +0200

    heap_sinkroot refactor

--- heap.h
+++ heap.h
@@ -6,36 +6,6 @@
 /* returns true if a > b */
 typedef bool (*heap_gt)(const E a, const E b);
 
-/* make a heap from random array, arr[0] is max element according to gt */
-void heap_make(E *arr, size_t len, heap_gt gt)
-{
-	if (len < 2) {
-		return;
-	}
-	size_t last_parent = (len - 1 - 1) / 2;
-	for (size_t pp1 = last_parent + 1; pp1 > 0; pp1--) {
-		size_t p = pp1 - 1;
-		size_t imax = p;
-		for(;;) {
-			size_t c = 2 * p + 1;
-			if (c < len && gt(arr[c], arr[imax])) {
-				imax = c;
-			}
-			c++;
-			if (c < len && gt(arr[c], arr[imax])) {
-				imax = c;
-			}
-			if (imax == p) {
-				break;
-			}
-			E tmp = arr[imax];
-			arr[imax] = arr[p];
-			arr[p] = tmp;
-			p = imax;
-		}
-	}
-}
-
 /* root of a heap is not max, sink it down until heap is restored */
 void heap_sinkroot(E *arr, size_t len, heap_gt gt, size_t r)
 {
@@ -50,11 +20,24 @@ void heap_sinkroot(E *arr, size_t len, heap_gt gt, size_t r)
 			m = c;
 		}
 		if (m == r) {
-			E tmp = arr[m];
-			arr[m] = arr[r];
-			arr[r] = tmp;
-			r = m;
+			break;
 		}
+		E tmp = arr[m];
+		arr[m] = arr[r];
+		arr[r] = tmp;
+		r = m;
+	}
+}
+
+/* make a heap from random array, arr[0] is max element according to gt */
+void heap_make(E *arr, size_t len, heap_gt gt)
+{
+	if (len < 2) {
+		return;
+	}
+	size_t last_parent = (len - 1 - 1) / 2;
+	for (size_t pp1 = last_parent + 1; pp1 > 0; pp1--) {
+		heap_sinkroot(arr, len, gt, pp1 - 1);
 	}
 }
 
