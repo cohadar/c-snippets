622373127cc8570151a5d962b324830bda67f4d1
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-09-30 07:19:48 +0200

    no circular

--- slist.h
+++ slist.h
@@ -1,102 +1,85 @@
 /*
- * Singly linked list. (circular, with head pivot)
+ * Singly linked list. (with dummy head)
  * Template letter: E
  * TODO: Storm.dll lists
  */
-typedef struct SList {
+typedef struct SNode {
 	E data;
-	struct SList *next;
+	struct SNode *next;
+} SNode;
+
+typedef struct {
+	struct SNode *_head;
+	struct SNode *_tail;
+	size_t _len;
 } SList;
 
 SList *SList_new() {
 	SList *o = malloc(sizeof(*o));
 	assert(o);
-	// Data is uninitialized in pivot.
-	// Empty list is represented as a pivot pointing to itself.
-	o->next = o;
+	SNode *dummy = malloc(sizeof(*dummy));
+	assert(dummy);
+	o->_head = dummy;
+	o->_tail = dummy;
+	o->_len = 0;
+	dummy->next = NULL;
 	return o;
 }
 
 void SList_delete(SList *o)
 {
-	assert(o);
-	SList *p = o->next;
-	while (p != o) {
-		SList *tmp = p->next;
+	SNode *p = o->_head;
+	while (p) {
+		SNode *tmp = p->next;
+		// paranoid nulling
+		p->next = NULL;
 		free(p);
 		p = tmp;
 	}
+	// paranoid nulling
+	o->_head = NULL;
+	o->_tail = NULL;
+	o->_len = 0;
 	free(o);
 }
 
 void SList_appendArray(SList *o, E *arr, size_t len)
 {
 	assert(len >= 0);
-	assert(o);
-	SList *p = o;
+	SNode *p = o->_tail;
 	for (size_t i = 0; i < len; i++) {
 		p->next = malloc(sizeof(*p->next));
 		assert(p->next);
 		p = p->next;
 		p->data = arr[i];
+		o->_len++;
 	}
-	p->next = o;
+	p->next = NULL;
+	o->_tail = p;
 }
 
 E *SList_mallocArray(SList *o, size_t *len)
 {
 	assert(o);
 	assert(len);
-	*len = 0;
+	*len = o->_len;
 	E *arr = NULL;
-	SList *p = o->next;
-	while (p != o) {
-		arr = realloc(arr, ++(*len));
-		assert(arr);
-		arr[*len - 1] = p->data;
+	if (o->_len > 0) {
+		arr = malloc(o->_len * sizeof(*arr));
+	}
+	SNode *p = o->_head->next;
+	size_t i = 0;
+	while (p) {
+		arr[i++] = p->data;
 		p = p->next;
 	}
+	assert(i == o->_len);
 	return arr;
 }
 
-// TODO: there is an error is here, tail is not updated but it should be.
-SList* _SList_insertInSorted(SList *head, SList *tail, SList *toInsert)
-{
-	if (toInsert->data > head->data) {
-		SList *l = head;
-		SList *r = head->next;
-		while (r != head) {
-			if (toInsert->data > r->data) {
-				l = r;
-				r = r->next;
-			} else {
-				l->next = toInsert;
-				toInsert->next = r;
-				return head;
-			}
-		}
-		l->next = toInsert;
-		toInsert->next = head;
-		return head;
-	} else {
-		toInsert->next = head;
-		tail->next = toInsert;
-		return toInsert;
-	}
-}
-
-SList* SList_insertionSort(SList *o)
-{
-	assert(o);
-	if (o->next == o) return o;
-	SList *head = o;
-	SList *tail = o;
-	SList *p = o->next;
-	head->next = head;
-	while (p != o) {
-		SList *toInsert = p;
-		p = p->next;
-		head = _SList_insertInSorted(head, tail, toInsert);
-	}
-	return head;
-}
+// SList* SList_insertionSort(SList *o)
+// {
+// 	if (o->_len < 2) return o;
+// 	SNode *h = o->_head;
+// 	SNode *t = o->_head->next;
+// }
--- slist_test.c
+++ slist_test.c
@@ -19,24 +19,24 @@ void testToFromArray()
 	SList_delete(lst);
 }
 
-void testInsertionSort() {
-	char *arr = "2578419063";
-	size_t len = strlen(arr);
-	SList *lst = SList_new();
-	SList_appendArray(lst, arr, len);
-	lst = SList_insertionSort(lst);
-	char *actual = SList_mallocArray(lst, &len);
-	for (int i = 0; i < 10; i++) {
-		assert(i + '0' == actual[i]);
-	}
-	free(actual);
-	SList_delete(lst);
-}
+// void testInsertionSort() {
+// 	char *arr = "2578419063";
+// 	size_t len = strlen(arr);
+// 	SList *lst = SList_new();
+// 	SList_appendArray(lst, arr, len);
+// 	lst = SList_insertionSort(lst);
+// 	char *actual = SList_mallocArray(lst, &len);
+// 	for (int i = 0; i < 10; i++) {
+// 		assert(i + '0' == actual[i]);
+// 	}
+// 	free(actual);
+// 	SList_delete(lst);
+// }
 
 int main(int argc, char const *argv[])
 {
 	testToFromArray();
-	testInsertionSort();
+	//testInsertionSort();
 	printf("All Ok.\n");
 	return 0;
 }
