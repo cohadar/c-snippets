863a6463cc89adbd9cc4dd7cfef0aa2309afdbdf
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-10-05 07:50:01 +0200

    HashMap_remove

--- hashmap.h
+++ hashmap.h
@@ -12,6 +12,7 @@ typedef size_t (*HashMap_hash)(K key);
 
 typedef struct {
 	size_t cap;
+	size_t size;
 	HashMap_hash hash;
 	K null_key;
 	V null_value;
@@ -25,6 +26,7 @@ HashMap *HashMap_new(size_t cap, HashMap_hash hash, K null_key, V null_value)
 	HashMap *o = malloc(sizeof(*o));
 	assert(o);
 	o->cap = cap;
+	o->size = 0;
 	o->hash = hash;
 	o->null_key = null_key;
 	o->null_value = null_value;
@@ -42,70 +44,102 @@ void HashMap_delete(HashMap *o)
 {
 	assert(o);
 	for (size_t i = 0; i < o->cap; i++) {
-		HashEntry *p = o->data[i].next;
-		while (p) {
-			HashEntry *t = p->next;
-			free(p);
-			p = t;
+		HashEntry *e = o->data[i].next;
+		while (e) {
+			HashEntry *t = e->next;
+			free(e);
+			e = t;
 		}
 	}
 	free(o->data);
 	o->cap = 0;
+	o->size = 0;
 	free(o);
 }
 
+/*
+ * @returns old value on key if found else null_value.
+ */
 V HashMap_put(HashMap *o, K key, V value)
 {
 	assert(key != o->null_key);
 	size_t i = o->hash(key);
 	assert(i < o->cap);
-	if (o->data[i].key == key) {
-		V old = o->data[i].value;
-		o->data[i].value = value;
-		return old;
-	} else if (o->data[i].key == o->null_key) {
-		o->data[i].key = key;
-		o->data[i].value = value;
-		return o->null_value;
-	} else {
-		HashEntry *e = o->data[i].next;
-		while (e) {
-			if (e->key == key) {
-				V old = e->value;
-				e->value = value;
-				return old;
-			} else {
-				e = e->next;
-			}
+	HashEntry *e = o->data + i;
+	while (e) {
+		if (e->key == key) {
+			V old = e->value;
+			e->value = value;
+			return old;
+		} else {
+			e = e->next;
 		}
-		e = malloc(sizeof(*e));
-		assert(e);
-		e->key = key;
-		e->value = value;
-		e->next = o->data[i].next;
-		o->data[i].next = e;
-		return o->null_value;
 	}
+	while (e) {
+		if (e->key == o->null_key) {
+			e->value = value;
+			o->size++;
+			return o->null_value;
+		} else {
+			e = e->next;
+		}
+	}
+	e = malloc(sizeof(*e));
+	assert(e);
+	e->key = key;
+	e->value = value;
+	e->next = o->data[i].next;
+	o->data[i].next = e;
+	o->size++;
+	return o->null_value;
 }
 
+/*
+ * @returns value on key if found else null_value.
+ */
 V HashMap_get(HashMap *o, K key)
 {
 	assert(key != o->null_key);
 	size_t i = o->hash(key);
 	assert(i < o->cap);
-	if (o->data[i].key == key) {
-		return o->data[i].value;
-	} else if (o->data[i].key == o->null_key) {
-		return o->null_value;
-	} else {
-		HashEntry *e = o->data[i].next;
-		while (e) {
-			if (e->key == key) {
-				return e->value;
-			} else {
-				e = e->next;
-			}
+	HashEntry *e = o->data + i;
+	while (e) {
+		if (e->key == key) {
+			return e->value;
+		} else {
+			e = e->next;
 		}
-		return o->null_value;
 	}
+	return o->null_value;
+}
+
+/*
+ * @returns removed value if key found else null_value.
+ * remove does not release any entries, this keeps iterators safe.
+ * if you need to reclaim bucket memory your capacity was not correct anyways.
+ */
+V HashMap_remove(HashMap *o, K key)
+{
+	assert(key != o->null_key);
+	size_t i = o->hash(key);
+	assert(i < o->cap);
+	HashEntry *e = o->data + i;
+	while (e) {
+		if (e->key == key) {
+			V removed = e->value;
+			e->key = o->null_key;
+			e->value = o->null_value;
+			o->size--;
+			return removed;
+		} else {
+			e = e->next;
+		}
+	}
+	return o->null_value;
+}
+
+/***/
+size_t HashMap_size(HashMap *o)
+{
+	return o->size;
 }
--- hashmap_test.c
+++ hashmap_test.c
@@ -37,13 +37,30 @@ void saturation_test() {
 
 	HashMap *map = HashMap_new(1024, hash1024, NULL, -1);
 
-	for (size_t i = 0; i < 4096; i++) {
-		HashMap_put(map, keys[i], i);
-	}
-
-	for (size_t i = 0; i < 4096; i++) {
-		assert(HashMap_get(map, keys[i]) == i);
-		printf("%d : %s\n", HashMap_get(map, keys[i]), keys[i]);
+	for (size_t k = 1; k <= 10; k++) {
+		// put all
+		for (size_t i = 0; i < 4096; i++) {
+			HashMap_put(map, keys[i], i);
+			assert(HashMap_size(map) == i + 1);
+		}
+		// get all
+		for (size_t i = 0; i < 4096; i++) {
+			assert(HashMap_get(map, keys[i]) == i);
+		}
+		// update all
+		for (size_t i = 0; i < 4096; i++) {
+			HashMap_put(map, keys[i], i + k);
+			assert(HashMap_size(map) == 4096);
+		}
+		// get all
+		for (size_t i = 0; i < 4096; i++) {
+			assert(HashMap_get(map, keys[i]) == i + k);
+		}
+		// delete all
+		for (size_t i = 0; i < 4096; i++) {
+			assert(HashMap_remove(map, keys[i]) == i + k);
+			assert(HashMap_size(map) == 4096 - 1 - i);
+		}
 	}
 
 	HashMap_delete(map);
