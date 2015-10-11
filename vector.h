e1dbd94a6a4034e0311790d563198294cfdda87c
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-10-11 14:30:39 +0200

    vector update

--- vector.h
+++ vector.h
@@ -1,45 +1,95 @@
 /*
- * Resizeable array.
- * Template letter: E
+ * Resizeable array. Interface based on Python list.
  */
 typedef struct {
-	E *data;
-	size_t length;
+	void **data;
+	size_t len;
+	size_t cap;
 } Vector;
+
+typedef void* (*Vector_mapfunc)(void *element, void *closure);
+typedef void (*Vector_freefunc)(void *element);
 
 Vector *Vector_new()
 {
 	Vector *o = malloc(sizeof(*o));
 	assert(o);
-	o->length = 0;
+	o->len = 0;
+	o->cap = 0;
 	o->data = NULL;
 	return o;
 }
 
-void Vector_delete(Vector *o)
-{
+void Vector_clear(Vector *o, Vector_freefunc func) {
+	assert(o);
 	if (o->data) {
+		if (func) {
+			for (size_t i = 0; i < o->len; i++) {
+				func(o->data[i]);
+			}
+		}
 		free(o->data);
 		o->data = NULL;
 	}
+	o->len = 0;
+	o->cap = 0;
+}
+
+void Vector_delete(Vector *o, Vector_freefunc func)
+{
+	assert(o);
+	Vector_clear(o, func);
 	free(o);
 }
 
-void Vector_push(Vector *o, E element)
+void Vector_append(Vector *o, void *element)
 {
-	o->data = realloc(o->data, ++o->length * sizeof(E));
-	assert(o->data);
-	o->data[o->length - 1] = element;
+	assert(o);
+	if (o->len == o->cap) {
+		o->cap = (o->cap == 0) ? 256 : o->cap * 2;
+		o->data = realloc(o->data, o->cap * sizeof(*o->data));
+		assert(o->data);
+	}
+	o->data[o->len] = element;
+	o->len++;
 }
 
-E Vector_pop(Vector *o)
+void Vector_extend(Vector *o, Vector *v)
 {
-	assert(o->length > 0);
-	E ret = o->data[--o->length];
-	if (o->length > 0) {
-		o->data = realloc(o->data, o->length * sizeof(E));
+	assert(o);
+	for (size_t i = 0; i < v->len; i++) {
+		Vector_append(o, v->data[i]);
+	}
+}
+
+void *Vector_pop(Vector *o)
+{
+	assert(o);
+	assert(o->len > 0);
+	void *ret = o->data[--o->len];
+	if (o->len > 0 && 2 * o->len < o->cap) {
+		o->cap /= 2;
+		o->data = realloc(o->data, o->cap * sizeof(*o->data));
 		assert(o->data);
 	}
 	return ret;
 }
 
+size_t Vector_length(Vector *o)
+{
+	assert(o);
+	return o->len;
+}
+
+void *Vector_map(Vector *o, Vector_mapfunc func, void *closure)
+{
+	assert(o);
+	Vector *ret = Vector_new();
+	for (size_t i = 0; i < o->len; i++) {
+		void *mapped = func(o->data[i], closure);
+		if (mapped) {
+			Vector_append(ret, mapped);
+		}
+	}
+	return ret;
+}
--- vector_test.c
+++ vector_test.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
-#include "asrt/asrt.h"
+#include <stdbool.h>
+
 
 #define E int
 #include "vector.h"
@@ -8,17 +10,40 @@
 
 #define MEGA (1024 * 1024)
 
+void *box_int(int x)
+{
+	int *ret = malloc(sizeof(*ret));
+	assert(ret);
+	*ret = x;
+	return ret;
+}
+
+int unbox_int(int *i)
+{
+	int ret = *i;
+	free(i);
+	return ret;
+}
+
+void *square(void *e, void *closure)
+{
+	return box_int((*(int*)e) * (*(int*)e));
+}
+
 int main(int argc, char const *argv[])
 {
 	Vector *vec = Vector_new();
 	for (int i = 0; i < 10 * MEGA; i++) {
-		Vector_push(vec, i);
+		Vector_append(vec, box_int(i));
 	}
+	Vector *temp = vec;
+	vec = Vector_map(vec, square, NULL);
+	Vector_delete(temp, free);
 	for (int i = 10 * MEGA - 1; i >= 0; i--) {
-		int e = Vector_pop(vec);
-		ASRT_int(i, e);
+		int e = unbox_int(Vector_pop(vec));
+		assert(i * i == e);
 	}
-	Vector_delete(vec);
+	Vector_delete(vec, free);
 	printf("All Ok.\n");
 	return 0;
 }
